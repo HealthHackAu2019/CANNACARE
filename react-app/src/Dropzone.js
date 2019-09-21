c51ee21c8f10f2b5ab876bb6696be2308138199f
Matthew-Burfield <m1.burfield@gmail.com>
2019-09-21 15:10:17 +1000

    basic drop styling

--- react-app/src/Dropzone.js
+++ react-app/src/Dropzone.js
@@ -2,11 +2,32 @@ import React, { useCallback } from "react";
 import styled from "styled-components";
 import { useDropzone } from "react-dropzone";
 
+import UploadCloud from "./svg/UploadCloud";
+
 const DropzoneStyled = styled.div`
   width: 480px;
   height: 300px;
   border: 1px dashed #71858c;
   border-radius: 8px;
+  div.display {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    color: #272e31;
+    div {
+      line-height: 48px;
+      color: white;
+      background-color: black;
+      padding: 0 17px;
+      border-radius: 4px;
+      :hover {
+        cursor: pointer;
+        opacity: 0.8;
+      }
+    }
+  }
 `;
 
 const Dropzone = () => {
@@ -16,19 +37,20 @@ const Dropzone = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <DropzoneStyled {...getRootProps()}>
       <input {...getInputProps()} />
       {isDragActive ? (
         <p>Drop the files here ...</p>
       ) : (
-        <div>
+        <div className="display">
+          <UploadCloud />
           <p>Drop Your HERC PDF Here</p>
           <div>
             <span>Or</span> Choose your file to upload
           </div>
         </div>
       )}
-    </div>
+    </DropzoneStyled>
   );
 };
 
--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -10,17 +10,38 @@ const Container = styled.div`
   right: 0;
   bottom: 0;
   section.top {
-    background-color: #71858c;
-    height: 50%;
+    height: 20%;
+    background-color: #272e31;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    color: white;
+    h2 {
+      padding: 0 0 11px;
+      margin: 0 20px;
+      border-bottom: 4px solid transparent;
+      :hover {
+        cursor: pointer;
+      }
+    }
+    h2.active {
+      border-bottom: 4px solid white;
+    }
   }
-  section.bottom: {
-    background-color: red;
+  section.bottom {
+    display: flex;
+    padding: 50px 0 0;
+    justify-content: center;
   }
 `;
 
 const Home = () => (
   <Container>
-    <section className="top"></section>
+    <section className="top">
+      <h2 className="active">I want to contribute to the database</h2>
+      <h2>I want to explore the database</h2>
+    </section>
     <section className="bottom">
       <Dropzone />
     </section>
