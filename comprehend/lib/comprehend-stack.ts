a4f04be8ccbe2242a07577920cfe67b337cb324d
Andrew Newton <andrewnewton@geniesolutions.com.au>
2019-09-21 16:59:47 +1000

    Initial setup with API endpoint for upload file

--- comprehend/lib/comprehend-stack.ts
+++ comprehend/lib/comprehend-stack.ts
@@ -2,17 +2,22 @@ import sns = require('@aws-cdk/aws-sns');
 import subs = require('@aws-cdk/aws-sns-subscriptions');
 import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
 
 export class ComprehendStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'ComprehendQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // defines an AWS Lambda resource
+    const uploadFile = new lambda.Function(this, 'UploadFile', {
+      runtime: lambda.Runtime.NODEJS_8_10,
+      code: lambda.Code.asset('lambda'),
+      handler: 'uploadfile.handler'
     });
 
-    const topic = new sns.Topic(this, 'ComprehendTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new apigw.LambdaRestApi(this, 'uploadfile', {
+      handler: uploadFile
+    });
   }
 }
