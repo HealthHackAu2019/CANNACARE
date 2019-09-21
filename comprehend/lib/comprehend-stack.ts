1aedb0bc714b926c841ecd31026550f780b292b3
Andrew Newton <andrewnewton@geniesolutions.com.au>
2019-09-21 19:19:01 +1000

    Large file uploads are not going to work!  Plan B

--- comprehend/lib/comprehend-stack.ts
+++ comprehend/lib/comprehend-stack.ts
@@ -3,21 +3,35 @@ import subs = require('@aws-cdk/aws-sns-subscriptions');
 import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
 import lambda = require('@aws-cdk/aws-lambda');
-import apigw = require('@aws-cdk/aws-apigateway');
+import apigateway = require('@aws-cdk/aws-apigateway');
+import s3 = require('@aws-cdk/aws-s3');
 
 export class ComprehendStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // defines an AWS Lambda resource
-    const uploadFile = new lambda.Function(this, 'UploadFile', {
+    const bucket = new s3.Bucket(this, 'comprehend');
+    
+    const uploadFileFunction = new lambda.Function(this, 'UploadFile', {
       runtime: lambda.Runtime.NODEJS_8_10,
       code: lambda.Code.asset('lambda'),
-      handler: 'uploadfile.handler'
+      handler: 'uploadfile.main',
+      environment: {
+        BUCKET: bucket.bucketName
+      }
     });
 
-    new apigw.LambdaRestApi(this, 'uploadfile', {
-      handler: uploadFile
+    bucket.grantReadWrite(uploadFileFunction);
+
+    const mainApi = new apigateway.RestApi(this, "comprehension", {
+      restApiName: "fileupload",
+      description: "This coordinates uploading the file, comprehending it and returning the results"
     });
+
+    const apiUploadFileLambda = new apigateway.LambdaIntegration(uploadFileFunction, {
+      requestTemplates: { "application/json": '{ "statusCode": "200" }' }
+    });
+
+    mainApi.root.addMethod("POST", apiUploadFileLambda); 
   }
 }
